d8acbf970315ea4b084d1cd94a8304acad8e7a37
Nick Tammadge <nicktammadge@gmail.com>
2024-10-07 18:31:38 -0700

    Parse imported files from an opened file

--- src/msbuildls.LanguageServer/Handlers/TextDocumentHandler.cs
+++ src/msbuildls.LanguageServer/Handlers/TextDocumentHandler.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.Extensions.Logging;
 using msbuildls.LanguageServer.Symbols;
+using msbuildls.LanguageServer.Symbols.MSBuild;
 using OmniSharp.Extensions.LanguageServer.Protocol;
 using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
 using OmniSharp.Extensions.LanguageServer.Protocol.Document;
@@ -45,12 +49,60 @@ internal class TextDocumentHandler : TextDocumentSyncHandlerBase
         _logger.LogInformation("Opened file: {filePath}", filePath);
 
         var docSymbols = _symbolFactory.ParseDocument(request.TextDocument);
-        if (docSymbols != null)
+        if (docSymbols == null)
         {
-            _symbolProvider.AddOrUpdateSymbols(filePath, docSymbols);
+            return Unit.Task;
         }
+        _symbolProvider.AddOrUpdateSymbols(filePath, docSymbols);
+
+        var parsedFiles = new List<string>() { filePath };
+        HandleImports(filePath, parsedFiles, docSymbols);
 
         return Unit.Task;
+    }
+
+    private void HandleImports(string importingFile, List<string> parsedFiles, Project fileSymbols)
+    {
+        var imports = new List<Import>();
+        imports.AddRange(fileSymbols.Imports ?? []);
+        imports.AddRange(fileSymbols.ImportGroups?.SelectMany(importGroup => importGroup.Imports ?? []) ?? []);
+
+        if (imports.Count == 0)
+        {
+            _logger.LogInformation("Not imports found in file {filePath}", importingFile);
+            return;
+        }
+
+        _logger.BeginScope("Imports found in {filePath}", importingFile);
+        var importingFileDirectory = new FileInfo(importingFile).Directory.FullName; // TODO: figure out when this could be null
+
+        foreach (var import in imports) // TODO: Order by appearance in the file
+        {
+            var importPath = import.Project;
+            if (!Path.IsPathFullyQualified(importPath))
+            {
+                importPath = Path.GetFullPath(Path.Combine(importingFileDirectory, importPath));
+            }
+
+            if (!Path.Exists(importPath))
+            {
+                _logger.LogInformation("Unable to parse file data for {filePath}. The file doesn't exist", importPath);
+                continue;
+            }
+            if (parsedFiles.Contains(importPath))
+            {
+                _logger.LogInformation("Skipped parsing of {filePath} because it has already been parsed", importPath);
+                continue;
+            }
+
+            var importSymbols = _symbolFactory.ParseFile(importPath);
+            parsedFiles.Add(importPath);
+            if (importSymbols != null)
+            {
+                _symbolProvider.AddOrUpdateSymbols(importPath, importSymbols);
+                HandleImports(importPath, parsedFiles, importSymbols);
+            }
+        }
     }
 
     public override Task<Unit> Handle(DidChangeTextDocumentParams request, CancellationToken cancellationToken)
--- test/msbuildls.Tests/Handlers/TextDocumentHandlerTests.cs
+++ test/msbuildls.Tests/Handlers/TextDocumentHandlerTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging.Abstractions;
@@ -83,5 +84,265 @@ public class TextDocumentHandlerTests
 
         mockSymbolFactory.Verify(factory => factory.ParseDocument(It.IsAny<TextDocumentItem>()), Times.Once);
         mockSymbolProvider.Verify(provider => provider.AddOrUpdateSymbols(It.IsAny<string>(), It.IsAny<Project>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task OpenDocumentDoesDeserializeImportedFilesWithFullyQualifiedPathsAsync()
+    {
+        var openedFilePath = GetFullyQualifiedTestDataFilePath("opened.targets");
+        var importedFilePath = GetFullyQualifiedTestDataFilePath("imported.targets");
+
+        var mockSymbolFactory = new Mock<ISymbolFactory>();
+        mockSymbolFactory
+            .Setup(factory => factory.ParseDocument(It.IsAny<TextDocumentItem>()))
+            .Callback<TextDocumentItem>((textDocument) =>
+            {
+                Assert.NotNull(textDocument);
+                Assert.Equal(openedFilePath, textDocument.Uri.ToUri().LocalPath);
+            })
+            .Returns(new Project()
+            {
+                Imports = [
+                    new Import()
+                    {
+                        Project = importedFilePath
+                    }
+                ]
+            });
+        mockSymbolFactory
+            .Setup(factory => factory.ParseFile(It.IsAny<string>()))
+            .Callback<string>((filePath) =>
+            {
+                Assert.Equal(importedFilePath, filePath);
+            })
+            .Returns(new Project());
+
+        var mockSymbolProvider = new Mock<ISymbolProvider>();
+        var handler = new TextDocumentHandler(NullLogger<TextDocumentHandler>.Instance, mockSymbolFactory.Object, mockSymbolProvider.Object);
+
+        var request = new DidOpenTextDocumentParams()
+        {
+            TextDocument = new TextDocumentItem()
+            {
+                Uri = new Uri(openedFilePath),
+                Text = string.Empty // Real content is unnecessary because the symbol factory is mocked
+            }
+        };
+
+        var result = await handler.Handle(request, new CancellationToken());
+
+        mockSymbolFactory.Verify(factory => factory.ParseDocument(It.IsAny<TextDocumentItem>()), Times.Once);
+        mockSymbolFactory.Verify(factory => factory.ParseFile(It.IsAny<string>()), Times.Once);
+        mockSymbolProvider.Verify(provider => provider.AddOrUpdateSymbols(It.IsAny<string>(), It.IsAny<Project>()), Times.Exactly(2));
+    }
+
+    [Fact]
+    public async Task OpenDocumentDoesDeserializeImportedFilesWithRelativePathsAsync()
+    {
+        var openedFilePath = GetFullyQualifiedTestDataFilePath("opened.targets");
+
+        var mockSymbolFactory = new Mock<ISymbolFactory>();
+        mockSymbolFactory
+            .Setup(factory => factory.ParseDocument(It.IsAny<TextDocumentItem>()))
+            .Callback<TextDocumentItem>((textDocument) =>
+            {
+                Assert.NotNull(textDocument);
+                Assert.Equal(openedFilePath, textDocument.Uri.ToUri().LocalPath);
+            })
+            .Returns(new Project()
+            {
+                Imports = [
+                    new Import()
+                    {
+                        Project = @".\imported.targets"
+                    }
+                ]
+            });
+        mockSymbolFactory
+            .Setup(factory => factory.ParseFile(It.IsAny<string>()))
+            .Callback<string>((filePath) =>
+            {
+                Assert.Equal(GetFullyQualifiedTestDataFilePath("imported.targets"), filePath);
+            })
+            .Returns(new Project());
+
+        var mockSymbolProvider = new Mock<ISymbolProvider>();
+        var handler = new TextDocumentHandler(NullLogger<TextDocumentHandler>.Instance, mockSymbolFactory.Object, mockSymbolProvider.Object);
+
+        var request = new DidOpenTextDocumentParams()
+        {
+            TextDocument = new TextDocumentItem()
+            {
+                Uri = new Uri(openedFilePath),
+                Text = string.Empty // Real content is unnecessary because the symbol factory is mocked
+            }
+        };
+
+        var result = await handler.Handle(request, new CancellationToken());
+
+        mockSymbolFactory.Verify(factory => factory.ParseDocument(It.IsAny<TextDocumentItem>()), Times.Once);
+        mockSymbolFactory.Verify(factory => factory.ParseFile(It.IsAny<string>()), Times.Once);
+        mockSymbolProvider.Verify(provider => provider.AddOrUpdateSymbols(It.IsAny<string>(), It.IsAny<Project>()), Times.Exactly(2));
+    }
+
+    [Fact]
+    public async Task OpenDocumentDoesDeserializeImportedFilesFromImportedFilesAsync()
+    {
+        var openedFilePath = GetFullyQualifiedTestDataFilePath("opened.targets");
+        var importedFilePath = "imported.targets";
+        var nestedImportedFilePath = "nestedImport.targets";
+
+        var mockSymbolFactory = new Mock<ISymbolFactory>();
+        mockSymbolFactory
+            .Setup(factory => factory.ParseDocument(It.IsAny<TextDocumentItem>()))
+            .Callback<TextDocumentItem>((textDocument) =>
+            {
+                Assert.NotNull(textDocument);
+                Assert.Equal(openedFilePath, textDocument.Uri.ToUri().LocalPath);
+            })
+            .Returns(new Project()
+            {
+                Imports = [
+                    new Import()
+                    {
+                        Project = importedFilePath
+                    }
+                ]
+            });
+        mockSymbolFactory
+            .Setup(factory => factory.ParseFile(It.Is<string>((filePath) => filePath == GetFullyQualifiedTestDataFilePath(importedFilePath))))
+            .Returns(new Project()
+            {
+                Imports = [
+                    new Import()
+                    {
+                        Project = nestedImportedFilePath
+                    }
+                ]
+            });
+        mockSymbolFactory
+            .Setup(factory => factory.ParseFile(It.Is<string>((filePath) => filePath == GetFullyQualifiedTestDataFilePath(nestedImportedFilePath))))
+            .Returns(new Project());
+
+        var mockSymbolProvider = new Mock<ISymbolProvider>();
+        var handler = new TextDocumentHandler(NullLogger<TextDocumentHandler>.Instance, mockSymbolFactory.Object, mockSymbolProvider.Object);
+
+        var request = new DidOpenTextDocumentParams()
+        {
+            TextDocument = new TextDocumentItem()
+            {
+                Uri = new Uri(openedFilePath),
+                Text = string.Empty // Real content is unnecessary because the symbol factory is mocked
+            }
+        };
+
+        var result = await handler.Handle(request, new CancellationToken());
+
+        mockSymbolFactory.Verify(factory => factory.ParseDocument(It.IsAny<TextDocumentItem>()), Times.Once);
+        mockSymbolFactory.Verify(factory => factory.ParseFile(It.IsAny<string>()), Times.Exactly(2));
+        mockSymbolProvider.Verify(provider => provider.AddOrUpdateSymbols(It.IsAny<string>(), It.IsAny<Project>()), Times.Exactly(3));
+    }
+
+    [Fact]
+    public async Task OpenDocumentDoesNotInfiniteLoopIfDocumentAttemptsToImportItselfAsync()
+    {
+        var openedFilePath = @"C:\Opened\targets.targets";
+
+        var mockSymbolFactory = new Mock<ISymbolFactory>();
+        mockSymbolFactory
+            .Setup(factory => factory.ParseDocument(It.IsAny<TextDocumentItem>()))
+            .Callback<TextDocumentItem>((textDocument) =>
+            {
+                Assert.NotNull(textDocument);
+                Assert.Equal(openedFilePath, textDocument.Uri.ToUri().LocalPath);
+            })
+            .Returns(new Project()
+            {
+                Imports = [
+                    new Import()
+                    {
+                        Project = openedFilePath // Intentionally the same path in order to test self-reference/infinite loop avoidance
+                    }
+                ]
+            });
+
+        var mockSymbolProvider = new Mock<ISymbolProvider>();
+        var handler = new TextDocumentHandler(NullLogger<TextDocumentHandler>.Instance, mockSymbolFactory.Object, mockSymbolProvider.Object);
+
+        var request = new DidOpenTextDocumentParams()
+        {
+            TextDocument = new TextDocumentItem()
+            {
+                Uri = new Uri(openedFilePath),
+                Text = string.Empty // Real content is unnecessary because the symbol factory is mocked
+            }
+        };
+
+        var result = await handler.Handle(request, new CancellationToken());
+
+        mockSymbolFactory.Verify(factory => factory.ParseDocument(It.IsAny<TextDocumentItem>()), Times.Once);
+        mockSymbolFactory.Verify(factory => factory.ParseFile(It.IsAny<string>()), Times.Never);
+        mockSymbolProvider.Verify(provider => provider.AddOrUpdateSymbols(It.IsAny<string>(), It.IsAny<Project>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task OpenDocumentDeserializesImportFromImportGroupsAsync()
+    {
+        var openedFilePath = GetFullyQualifiedTestDataFilePath("opened.targets");
+        var importedFilePath = GetFullyQualifiedTestDataFilePath("imported.targets");
+
+        var mockSymbolFactory = new Mock<ISymbolFactory>();
+        mockSymbolFactory
+            .Setup(factory => factory.ParseDocument(It.IsAny<TextDocumentItem>()))
+            .Callback<TextDocumentItem>((textDocument) =>
+            {
+                Assert.NotNull(textDocument);
+                Assert.Equal(openedFilePath, textDocument.Uri.ToUri().LocalPath);
+            })
+            .Returns(new Project()
+            {
+                ImportGroups = [
+                    new ImportGroup()
+                    {
+                        Imports = [
+                            new Import()
+                            {
+                                Project = importedFilePath
+                            }
+                        ]
+                    }
+                ]
+            });
+        mockSymbolFactory
+            .Setup(factory => factory.ParseFile(It.IsAny<string>()))
+            .Callback<string>((filePath) =>
+            {
+                Assert.Equal(importedFilePath, filePath);
+            })
+            .Returns(new Project());
+
+        var mockSymbolProvider = new Mock<ISymbolProvider>();
+        var handler = new TextDocumentHandler(NullLogger<TextDocumentHandler>.Instance, mockSymbolFactory.Object, mockSymbolProvider.Object);
+
+        var request = new DidOpenTextDocumentParams()
+        {
+            TextDocument = new TextDocumentItem()
+            {
+                Uri = new Uri(openedFilePath),
+                Text = string.Empty // Real content is unnecessary because the symbol factory is mocked
+            }
+        };
+
+        var result = await handler.Handle(request, new CancellationToken());
+
+        mockSymbolFactory.Verify(factory => factory.ParseDocument(It.IsAny<TextDocumentItem>()), Times.Once);
+        mockSymbolFactory.Verify(factory => factory.ParseFile(It.IsAny<string>()), Times.Once);
+        mockSymbolProvider.Verify(provider => provider.AddOrUpdateSymbols(It.IsAny<string>(), It.IsAny<Project>()), Times.Exactly(2));
+    }
+
+    private string GetFullyQualifiedTestDataFilePath(string fileName)
+    {
+        var relativeDirectory = @"..\..\..\..\testData\TextDocumentHandlerTests";
+        return Path.GetFullPath(Path.Combine(relativeDirectory, fileName));
     }
 }
